c27b7a795dc68cf8c37e1a81776c1a62157ba1b2
Krish Patel <142641352+PATELKRISH9@users.noreply.github.com>
2025-06-21 18:15:14 +0530

    Update Navbar.js

--- src/comonets/Navbar.js
+++ src/comonets/Navbar.js
@@ -1,31 +1,48 @@
 import React, { Component } from 'react'
- 
+
 
 export class Navbar extends Component {
 
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">NewsMonkey</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>                                                   
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/about">About</a>
-        </li>
-        
-      </ul>
-     
-    </div>
-  </div>
-</nav>
+      <div className='k11'>
+        <div className='k1'>
+          <nav className="navbar navbar-expand-lg bg-body-" >
+            <div className="container-fluid">
+              <a className="navbar-brand" id='m'  href="/">NewsMonkey</a>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <a className="nav-link active" id='m' aria-current="page" href="">Home</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" id='m' href="/about">About</a>
+                  </li>
+                  <li className="nav-item ">
+                    <a className="nav-link"  id='m' href="/business">business</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" id='m' href="/entertainment">entertainment</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" id='m' href="/general">general</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" id='m' href="/health">health</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link"id='m' href="/science">science</a>
+                  </li>
+
+                </ul>
+
+              </div>
+            </div>
+          </nav>
+        </div>
       </div>
     )
   }
