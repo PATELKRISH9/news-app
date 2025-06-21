7b74b6b8ce05831f3ccc8f75238713396712464e
Krish Patel <142641352+PATELKRISH9@users.noreply.github.com>
2025-06-21 18:16:25 +0530

    Update Newsitem.js

--- src/comonets/Newsitem.js
+++ src/comonets/Newsitem.js
@@ -2,24 +2,24 @@ import React, { Component } from 'react'
 
 export class Newsitem extends Component {
   render() {
-     let {title,description,imageUrl,NewsUrl}=this.props;
+    let { title, description, image, NewsUrl } = this.props;
     return (
-       
+
       <div className='my-3'>
-      <div className="card" style={{ width: "18rem" }}>
-  <img src={imageUrl} className="card-img-top" alt="..." />
-  <div className="card-body">
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">
-      {description}
-    </p>
-    <a href={NewsUrl}     className="btn btn-primary">
-      Read More
-    </a>
-  </div>
-</div>
-</div>
-      
+        <div className="card" >
+          <img src={!image ? "https://cdn.ndtv.com/common/images/ogndtv.png" : image} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">
+              {!description ? "MOBILES & TABLETS News: Oppo announces AI availability on smartphones 4, partnering with tech giants for AI features like image process" : description}
+            </p>
+            <a href={NewsUrl}   className="btn btn-primary">
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+
     )
   }
 }
